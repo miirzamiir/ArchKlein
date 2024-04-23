4cc5d4b850f0e12810faaedf6fc3380f7c86bf5d
miirzamiir <miirzamiir@gmail.com>
2024-04-23 10:57:16 +0330

    getting things ready to commit

--- ArchKlein/settings.py
+++ ArchKlein/settings.py
@@ -12,16 +12,21 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h*s29))z6)@vh9$0@ypal4i)@+18#t+z+p-055zwbi%@=mg2_e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,6 +84,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER': ,
+        # 'PASSWORD': ,
+        # 'PORT':
     }
 }
 
